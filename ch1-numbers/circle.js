69e459f8ffd7e262a1481e215dc5b471517ef699
Takis_tk <takistsekos@outlook.com>
2026-07-09 18:18:12 +0300

    circle

--- ch1-numbers/circle.js
+++ ch1-numbers/circle.js
@@ -2,4 +2,5 @@ const radius = 5;
 
 const area = Math.PI * radius ** 2;
 
-console.log(`Ciircle with radius ${radius} has area ${area.toFixed(2)}`)
+console.log(`Ciircle with radius ${radius} has area ${area.toFixed(2)})
+cm\u00B2`)
